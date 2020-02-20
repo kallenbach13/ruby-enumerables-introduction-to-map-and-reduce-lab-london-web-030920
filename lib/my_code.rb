af20f5b16c16bb4488229f9cdf45aee579b953ab
Isak Kallenbach <kallenbach13@gmail.com>
2020-02-20 15:46:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,6 +2,7 @@
 def map_to_negativize(source_array)
   counter = 0 
   negative_total = []
+  
   while counter < source_array.length
   negative_total << source_array[counter] * -1
   counter += 1
@@ -12,6 +13,7 @@ end
 def map_to_no_change(source_array)
   counter = 0
   pointless_array = []
+  
   while counter < source_array.length
   pointless_array << source_array[counter]
   counter += 1
@@ -22,6 +24,7 @@ end
 def map_to_double(source_array)
   counter = 0
   double_array = []
+  
   while counter < source_array.length
   double_array << source_array[counter] * 2
   counter += 1
@@ -33,7 +36,7 @@ def map_to_square(source_array)
   counter = 0
   square_array = []
   
-  while counter < 
+  while counter < source_array
   
   counter += 1
   end
