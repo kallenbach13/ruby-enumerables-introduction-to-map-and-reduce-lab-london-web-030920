70b06853b3e776018601c79ca212d18658d9d3b9
Isak Kallenbach <kallenbach13@gmail.com>
2020-02-20 15:36:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map_to_negativize(source_array)
   counter = 0 
   negative_total = []
   while counter < source_array.length
-  negative_total += source_array * -1
+  negative_total += source_array[counter] * -1
   counter += 1
   end
   negative_total
